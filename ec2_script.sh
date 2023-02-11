bb9bcc5551f19c6caea2c14429a47177b030e3e3
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-11 19:04:45 +0200

    added commemnt

--- ec2_script.sh
+++ ec2_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo "Running bash script!"
 sudo apt-get update
 echo Qwerty0134! | docker login -u andrienkoms --password-stdin
 docker pull andrienkoms/final:latest
 #docker stop \$(docker ps -a -q)
 
-echo "Hello from bash script"
+
 
