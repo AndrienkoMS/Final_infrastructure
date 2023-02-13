58c0a70c234b3318ee560cfea2849dab32c91be9
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-13 14:45:25 +0200

    changed back

--- autoscaling.tf
+++ autoscaling.tf
@@ -12,8 +12,8 @@ resource "aws_launch_configuration" "l1-launch-config" {
 
 resource "aws_autoscaling_group" "l1-group-autoscaling" {
     name                      = "l1-group-autoscaling"
-    #vpc_zone_identifier       = ["subnet-033bbd9e872782bc2"]                    #(Optional) - The VPC zone identifier
-    vpc_zone_identifier       = [aws_security_group.l1-final-wordpress-sg.id]
+    vpc_zone_identifier       = ["subnet-033bbd9e872782bc2","subnet-0aaaa3f6dadcf369e"]                    #(Optional) - The VPC zone identifier
+    #vpc_zone_identifier       = [aws_security_group.l1-final-wordpress-sg.id]
     launch_configuration      = aws_launch_configuration.l1-launch-config.name  #(Optional) Name of the launch configuration to use
     min_size                  = 2                                               #(Required) Minimum size of the Auto Scaling Group
     max_size                  = 4                                               #(Required) Maximum size of the Auto Scaling Group.
