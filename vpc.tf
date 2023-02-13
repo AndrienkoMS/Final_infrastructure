2c410302d36b6f69410a3fa5b9411d3c042b21c8
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-13 19:57:21 +0200

    changed avialability zone us-west-1c => us-west-1a

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "l1vpc-public-1" {
     vpc_id = aws_vpc.l1-vpc.id
     cidr_block = "172.25.2.0/24"
     map_public_ip_on_launch = true
-    availability_zone = "us-west-1b"
+    availability_zone = "us-west-1a"
     tags = {
         Name ="l1vpc-public-1"
     }
@@ -24,7 +24,7 @@ resource "aws_subnet" "l1vpc-public-2" {
     vpc_id = aws_vpc.l1-vpc.id
     cidr_block = "172.25.3.0/24"
     map_public_ip_on_launch = true
-    availability_zone = "us-west-1c"
+    availability_zone = "us-west-1b"
     tags = {
         Name ="l1vpc-public-2"
     }
