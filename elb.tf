4e4a0463ed470cb5804b8520aab964a32a12b376
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-13 21:43:00 +0200

    added 22 port to ec2 instance sg

--- elb.tf
+++ elb.tf
@@ -69,6 +69,13 @@ resource "aws_security_group" "l1-instance-sg" {
     }
 
     ingress {
+        from_port   = 22 #ssh
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port = 8000
         to_port = 8000
         protocol = "tcp"
