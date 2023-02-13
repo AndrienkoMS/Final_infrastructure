168d7d5dc003843f321dbd7452ccc3a215a71f62
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-13 14:57:47 +0200

    deleted ClassicLink

--- autoscaling.tf
+++ autoscaling.tf
@@ -4,8 +4,7 @@ resource "aws_launch_configuration" "l1-launch-config" {
     instance_type   = var.instance_type
     #spot_price      = "0.001" #(Optional; Default: On-demand price) The maximum price to use for reserving spot instances.
     key_name        = aws_key_pair.l1_infrastructure_key.key_name
-    vpc_classic_link_security_groups = [aws_security_group.l1-final-wordpress-sg.id]
-
+    
   lifecycle {
     create_before_destroy = true
   }
@@ -16,7 +15,7 @@ resource "aws_autoscaling_group" "l1-group-autoscaling" {
     vpc_zone_identifier       = ["subnet-033bbd9e872782bc2","subnet-0aaaa3f6dadcf369e"]                    #(Optional) - The VPC zone identifier
     #vpc_zone_identifier       = [aws_security_group.l1-final-wordpress-sg.id]
     launch_configuration      = aws_launch_configuration.l1-launch-config.name  #(Optional) Name of the launch configuration to use
-    min_size                  = 2                                               #(Required) Minimum size of the Auto Scaling Group
+    min_size                  = 1                                               #(Required) Minimum size of the Auto Scaling Group
     max_size                  = 4                                               #(Required) Maximum size of the Auto Scaling Group.
     health_check_grace_period = 60                                              #Time (in seconds) after instance comes into service before checking health.
     health_check_type         = "EC2"                                           #(Optional) "EC2" or "ELB". Controls how health checking is done
