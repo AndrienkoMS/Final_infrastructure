e74a800709e5519823d3584df7ad722eea4330bb
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-14 18:27:04 +0200

    added depends_onto launchconfig

--- autoscaling.tf
+++ autoscaling.tf
@@ -9,7 +9,10 @@ resource "aws_launch_configuration" "l1-launch-config" {
     #security_groups = [aws_security_group.l1-final-wordpress-sg.id]
     user_data = file("ec2_script.sh")
     iam_instance_profile    = "${aws_iam_instance_profile.l1_infrastructure_ec2_profile.name}" 
-
+    depends_on = [
+        aws_db_instance.default
+    ]
+    
     lifecycle {
         create_before_destroy = true
     }
