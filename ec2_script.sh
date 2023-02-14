4ea62432b39f05451b1692fbe817b001c446d6d5
AndrienkoMS <108218480+AndrienkoMS@users.noreply.github.com>
2023-02-14 13:43:58 +0200

    changed user data

--- ec2_script.sh
+++ ec2_script.sh
@@ -8,8 +8,11 @@ docker rm $(docker ps -a -q) || echo "Nothing to delete"
 echo Qwerty0134! | docker login -u andrienkoms --password-stdin
 docker pull andrienkoms/final:latest
 
-docker run --env-file=myenvfile -p 8000:80 -d andrienkoms/final
+docker run --name wp-cont -e WORDPRESS_DB_HOST=wordpressdb.cde1mvsw4pqc.us-west-1.rds.amazonaws.com:3306 -e WORDPRESS_DB_USER=admin -e WORDPRESS_DB_PASSWORD=01340134 -p 8000:80 -d andrienkoms/final
+
+#docker run --env-file=myenvfile -p 8000:80 -d andrienkoms/final
 #docker run --name wp-cont -e WORDPRESS_DB_HOST=wordpressdb.cde1mvsw4pqc.us-west-1.rds.amazonaws.com:3306 -e WORDPRESS_DB_USER=admin -e WORDPRESS_DB_PASSWORD=01340134 -p 8000:80 -d andrienkoms/final
 #docker run --name wp-cont --env-file=application/myenvfile -p 8000:80 -d wp-image
+
 #connect to db from new instance
 #mysql -u admin -p01340134 -h wordpressdb.cde1mvsw4pqc.us-west-1.rds.amazonaws.com WordpressTest
